e2def844e8a0bc8aba18a7a8bfc346da680d951a
Nicolas Leigh <linze201@gmail.com>
2025-01-29 18:43:08 +0800

    add basic route structure

---
+++ app/(root)/page.tsx
@@ -0,0 +1,15 @@
+import { SignedIn, SignedOut, SignInButton, UserButton } from "@clerk/nextjs";
+
+export default function Home() {
+  // return (
+  //   <div>
+  //     <SignedOut>
+  //       <SignInButton />
+  //     </SignedOut>
+  //     <SignedIn>
+  //       <UserButton />
+  //     </SignedIn>
+  //   </div>
+  // );
+  return null;
+}
