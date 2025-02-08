a84bd173a24d5d3f7fb5d0bfad4f0b9c2a0eb460
Nicolas Leigh <linze201@gmail.com>
2025-02-08 18:44:50 +0800

    add environment variable support and Docker configuration for PostgreSQL

--- backend/cmd/main.go
+++ backend/cmd/main.go
@@ -4,22 +4,32 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+
+	"github.com/nicolasleigh/chat-app/env"
 )
 
 const version = "1.0.0"
 
 type config struct {
 	port int
+	db   dbConfig
 }
 
 type application struct {
 	config config
 }
 
+type dbConfig struct {
+	addr string
+}
+
 func main() {
 	app := &application{
 		config: config{
-			port: 8080,
+			port: env.GetInt("PORT", 8080),
+			db: dbConfig{
+				addr: env.GetString("DB_ADDR", "postgres://admin:adminpassword@localhost:5432/chat?sslmode=disable"),
+			},
 		},
 	}
 
